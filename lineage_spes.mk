f52e4cb8169faf78e2268eeffbd9b1f711d5e380
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-02-22 19:40:44 +0530

    spes: add priv-keys path

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -27,6 +27,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+include vendor/priv-keys/keys/keys.mk
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
